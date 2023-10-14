22bf41cac264f11a5d4f62115ac3069a44f1150d
DresdRam <snipermayv@gmail.com>
2023-10-14 14:47:18 +0300

    Upgraded Gradle Configurations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,35 +53,43 @@ android {
 
 dependencies {
 
-    //Dagger - Hilt
+    // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
 
-    //Retrofit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
-    //GSON
+    // GSON
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    //OkHttp
+    // OkHttp
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
+    // Core AndroidX dependencies
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
+
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.material:material:1.5.3")
+    implementation("androidx.compose.ui:ui-graphics:1.5.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.4")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Debugging
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
