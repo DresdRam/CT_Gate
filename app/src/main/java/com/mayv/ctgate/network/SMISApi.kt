69506024295d9650c37845155ca270597145288d
Mohamed Salah <semba.max@gmail.com>
2023-11-09 09:34:15 +0100

    Update SMISApi.kt

--- app/src/main/java/com/mayv/ctgate/network/SMISApi.kt
+++ app/src/main/java/com/mayv/ctgate/network/SMISApi.kt
@@ -12,6 +12,7 @@ interface SMISApi {
     @GET("soldier/gate-soldier")
     suspend fun getSoldier(@Query("national_id") nationalId: Long): Soldier
 
+     // CODE_REVIEW: since you developed the api yourself, it's better to cache these images on server side and just return the url.
     @GET("image/get")
     suspend fun getSoldierImage(@Query("national_id") nationalId: Long): ResponseBody
 
